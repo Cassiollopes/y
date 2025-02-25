91be6ca91fba75948b947a9671acaad9254f384e
cassio <cassio8976@gmail.com>
2025-02-25 15:49:12 -0300

    modified:   src/app/auth/callback/route.ts

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -3,10 +3,12 @@ import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url);
+  const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get("next") ?? "/";
+
+  const origin = process.env.NEXT_PUBLIC_SITE_URL || new URL(request.url).origin;
 
   if (code) {
     const supabase = await createClient();
