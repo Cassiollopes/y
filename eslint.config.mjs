e5ae79d8ab372e589c7fa824dcb77c1664666abd
cassio <cassio8976@gmail.com>
2025-02-25 15:11:53 -0300

    modified:   eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,8 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+eslintConfig.push({
+  ignores: ["src/utils/supabase/database.types.ts"],
+});
+
 export default eslintConfig;
