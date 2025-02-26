6dc9f9ba5820bc84c722d2c0e40b451f932766a4
cassio <cassio8976@gmail.com>
2025-02-26 08:52:34 -0300

    modified:   src/app/(sideBar)/layout.tsx

--- src/app/(sideBar)/layout.tsx
+++ src/app/(sideBar)/layout.tsx
@@ -1,5 +1,6 @@
 import SideBar from "@/components/sidebar";
 import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 
@@ -12,11 +13,15 @@ export default async function Layout({
 
   const {
     data: { user },
+    error,
   } = await supabase.auth.getUser();
+  if (error || !user) {
+    redirect("/login");
+  }
 
   return (
     <div className="flex min-h-screen max-w-[1260px] m-auto items-start justify-start xl:justify-start gap-2 xl:gap-3 relative">
-      <SideBar user={user!} />
+      <SideBar user={user} />
       <div className="flex flex-col h-screen items-center w-full lg:max-w-[600px] justify-start md:border-r md:border-l border-zinc-700/75">
         {children}
       </div>
