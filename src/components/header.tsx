bcc1a32e815dda6de7ed96d6411a59615f45b127
cassio <cassio8976@gmail.com>
2025-02-27 22:47:19 -0300

    modified:   src/components/header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,35 +1,46 @@
 "use client";
 
 import { useMotionValueEvent, useScroll, motion } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Header({
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
-  const { scrollYProgress } = useScroll();
   const [visible, setVisible] = useState(true);
+  const { scrollYProgress } = useScroll();
+  const hasInteracted = useRef(false); // Flag para evitar o primeiro trigger
+
+  useEffect(() => {
+    hasInteracted.current = false; // Reset ao montar
+  }, []);
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
+    if (!hasInteracted.current) {
+      hasInteracted.current = true; // Ignora o primeiro trigger
+      return;
+    }
+
     if (typeof current === "number") {
       const previous = scrollYProgress.getPrevious();
-      const direction = current - previous!;
-
+      const direction = current - (previous || 0);
       const pixelHeight = window.innerHeight;
       const pixelDifference = Math.abs(direction * pixelHeight);
 
-      if (scrollYProgress.get() === 0) {
+      if (direction > 0) {
+        setVisible(false);
+      } else if (
+        direction < 0 &&
+        pixelDifference >= 5 &&
+        Math.floor(pixelDifference) % 5 === 0
+      ) {
         setVisible(true);
-      } else {
-        if (direction < 0) {
-          if (pixelDifference >= 5 && Math.floor(pixelDifference) % 5 === 0) {
-            setVisible(true);
-          }
-        } else if (direction > 0) {
-          setVisible(false);
-        }
       }
     }
   });
+
+  useEffect(() => {
+    sessionStorage.setItem("visible", String(visible));
+  }, [visible]);
 
   return (
     <motion.div
