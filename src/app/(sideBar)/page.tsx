1870ae6af8518ab7baebe13b8fb20b333faa524f
cassio <cassio8976@gmail.com>
2025-03-08 06:00:05 -0300

    modified:   src/app/(sideBar)/page.tsx

--- src/app/(sideBar)/page.tsx
+++ src/app/(sideBar)/page.tsx
@@ -31,7 +31,7 @@ export default async function Home() {
 
   return (
     <div className="w-full">
-      <Header>
+      <Header className="max-md:relative">
         <div className="h-full py-4 relative">
           <p className="font-bold text-sm">Para você</p>
           <div className="absolute bottom-0 bg-sky-500 h-1 w-full rounded-full"></div>
