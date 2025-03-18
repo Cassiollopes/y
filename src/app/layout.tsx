7731b322482efe538e09fd38b424776382caab16
cassio <cassio8976@gmail.com>
2025-03-18 15:01:14 -0300

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="pt-BR" suppressHydrationWarning>
       <body className={`${inter.className} antialiased text-default/95 scroll-smooth`}>
         {children}
       </body>
