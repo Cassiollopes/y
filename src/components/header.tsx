54ea334f57a837acbb4b95a83b639f190b81a703
cassio <cassio8976@gmail.com>
2025-03-20 15:13:57 -0300

    modified:   src/components/header.tsx 	modified:   src/components/tweet/new-tweet-absolute.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export default function Header({
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={`sticky top-0 border-b px-4 ""-700/75 w-full backdrop-blur-lg bg-black/70 z-50 flex justify-around ${props.className}`}
+      className={`sticky top-0 border-b px-4 w-full backdrop-blur-lg bg-black/70 z-50 flex justify-around ${props.className}`}
     >
       {props.children}
     </div>
